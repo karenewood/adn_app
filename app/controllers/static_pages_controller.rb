7750f6dceb11350ee16d7bc0edcf7bc6b0b698ae
Karen Wood <northernyoga@gmail.com>
2013-07-10 17:47:59 -0400

    Add a static pages controller

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,10 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def decouvrir
+  end
+
+  def avantages
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 AdnApp::Application.routes.draw do
+  get "static_pages/home"
+
+  get "static_pages/decouvrir"
+
+  get "static_pages/avantages"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
