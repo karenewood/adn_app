9db0664be78ff1d9827bb719a471416d88e53ff8
Karen Wood <northernyoga@gmail.com>
2013-07-30 11:00:12 -0400

    Adding moving gif images

--- app/models/eval_test.rb
+++ app/models/eval_test.rb
@@ -11,6 +11,6 @@ class EvalTest < ActiveRecord::Base
   has_attached_file :image, styles: { medium: '200x200>' }
   validates_attachment_presence :image
   validates_attachment_size :image, :less_than => 5.megabytes
-  validates_attachment_content_type :image, :content_type => ['image/jpeg', 'image/png']
+  validates_attachment_content_type :image, :content_type => ['image/jpeg', 'image/png', 'image/gif']
 
 end
